2da50851784a488848cb79509727a0c35a217762
ttduy41 <ttduy41@gmail.com>
2026-05-03 10:45:49 +0700

    Add KEY_DATABASE with day-specific entries

---
+++ database.js
@@ -0,0 +1,15 @@
+const KEY_DATABASE = {
+    "day1": [
+        "1C1ACA28F878A6CB", 
+        "Fluorite-ADR-K9J2L7P",
+        "Fluorite-ADR-M1N5B8V"
+    ],
+    "day7": [
+        "Fluorite-VIP-7D-Q3W6E9R",
+        "Fluorite-VIP-7D-T4Y7U0I"
+    ],
+    "day30": [
+        "Fluorite-PRO-30D-P2O5S8A",
+        "Fluorite-PRO-30D-D1F4G7H"
+    ]
+};
